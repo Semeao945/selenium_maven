8d8dbe2a6d20250c3be4162d91bf5aa27a517afe
Flavio Semeao <f.semeao1987@gmail.com>
2022-02-09 21:18:26 -0300

    projeto selenium maven

---
+++ itau/src/main/java/selenium/itau/App.java
@@ -0,0 +1,13 @@
+package selenium.itau;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
